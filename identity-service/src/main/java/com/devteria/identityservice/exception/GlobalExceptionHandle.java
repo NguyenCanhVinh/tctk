d3d237d4c90b8ba77ea3bd8dd72b85d9dd66e793
Nguyen Canh Vinh <vinh.nguyencanh10@mobifone.vn>
2024-12-06 17:01:56 +0700

    fea: push code

--- identity-service/src/main/java/com/devteria/identityservice/exception/GlobalExceptionHandle.java
+++ identity-service/src/main/java/com/devteria/identityservice/exception/GlobalExceptionHandle.java
@@ -1,15 +1,22 @@
 package com.devteria.identityservice.exception;
 
 import com.devteria.identityservice.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.nio.file.AccessDeniedException;
+import java.util.Map;
+import java.util.Objects;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandle {
+
+  private static final String MIN_ATTRIBUTE = "min";
 
   @ExceptionHandler(value = Exception.class)
   ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException e){
@@ -33,10 +40,6 @@ public class GlobalExceptionHandle {
             .body(apiResponse);
   }
 
-  @ExceptionHandler(value = MethodArgumentNotValidException.class)
-  ResponseEntity<String> habdlingValidationException(MethodArgumentNotValidException e){
-    return  ResponseEntity.badRequest().body(e.getFieldError().getDefaultMessage());
-  }
 
   @ExceptionHandler(value = AccessDeniedException.class)
   ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException e){
@@ -48,5 +51,42 @@ public class GlobalExceptionHandle {
                     .message(errorCode.getMessage())
                     .build()
                     );
+  }
+
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException exception) {
+    String enumKey = exception.getFieldError().getDefaultMessage();
+
+    ErrorCode errorCode = ErrorCode.INVALID_KEY;
+    Map<String, Object> attributes = null;
+    try {
+      errorCode = ErrorCode.valueOf(enumKey);
+
+      var constraintViolation =
+        exception.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+      attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
+      log.info(attributes.toString());
+
+    } catch (IllegalArgumentException e) {
+
+    }
+
+    ApiResponse apiResponse = new ApiResponse();
+
+    apiResponse.setCode(errorCode.getCode());
+    apiResponse.setMessage(
+      Objects.nonNull(attributes)
+        ? mapAttribute(errorCode.getMessage(), attributes)
+        : errorCode.getMessage());
+
+    return ResponseEntity.badRequest().body(apiResponse);
+  }
+
+  private String mapAttribute(String message, Map<String, Object> attributes) {
+    String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+
+    return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
   }
 }
