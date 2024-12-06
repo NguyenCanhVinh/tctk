281b8c0d52fbec9c4265146c4a6dfc30a11d66a3
Nguyen Canh Vinh <vinh.nguyencanh10@mobifone.vn>
2024-12-06 09:11:50 +0700

    merge code authen

--- identity-service/src/main/java/com/devteria/identityservice/controller/PermissionController.java
+++ identity-service/src/main/java/com/devteria/identityservice/controller/PermissionController.java
@@ -11,15 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController ("/permission")
-@AllArgsConstructor
+@RestController
+@RequestMapping("/permission")
 public class PermissionController {
 
   private  final PermissionService permissionService;
+
+  public PermissionController(PermissionService permissionService) {
+    this.permissionService = permissionService;
+  }
 
   @PostMapping
   public ApiResponse<Permission> createPermission(@RequestBody @Valid PermissionRequest permissionRequest){
@@ -39,6 +44,7 @@ public class PermissionController {
     permissionService.deletePermission(permission);
     return ApiResponse.<Void>builder().build();
   }
+
 
 
 }
